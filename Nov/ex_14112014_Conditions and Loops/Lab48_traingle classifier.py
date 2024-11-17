18be5a6c89688db77e6294974175f0928c49a8d2
copellisd <sruthi.spr13@gmail.com>
2024-11-17 16:44:29 -0600

    for loop

--- Nov/ex_14112014_Conditions and Loops/Lab48_traingle classifier.py
+++ Nov/ex_14112014_Conditions and Loops/Lab48_traingle classifier.py
@@ -4,6 +4,6 @@ side3=int(input("enter side3 value"))
 if(side1==side2 and side2==side3):
     print("equaliteral traingle")
 elif(side1==side2 or side2==side3 or side1==side3):
-    print("isoceles traingle")
+    print("isoceles traingle ")
 else:
     print("scalene traingle")
